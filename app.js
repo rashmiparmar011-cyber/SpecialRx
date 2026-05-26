e60b55c04a0723deb4531e5b1ebe831a110d37cf
rashmiparmar011-cyber <rashmiparmar011@gmail.com>
2026-05-26 15:08:29 +0100

    ss

--- app.js
+++ app.js
@@ -1807,3 +1807,4 @@ function formatDate(date) {
 function formatDateShort(date) {
     return `${String(date.getDate()).padStart(2, '0')}/${String(date.getMonth() + 1).padStart(2, '0')}/${date.getFullYear()}`;
 }
+ 
